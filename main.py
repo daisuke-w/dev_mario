428fa835766b2de7269b1296aac5ab16e1a9340b
daisuke-w <w.daisuke.001@gmail.com>
2024-11-16 13:39:08 +0900

    画面表示

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
-import pygame
+import sys
+import pygame as pg
+
+# 画面サイズ
+WIDTH = 320
+HEIGHT = 270
+
+# タイル数
+TILE_X = 16
+TILE_Y = 14
 
 def main():
-  pass
+    '''メイン関数
+    '''
+    # Pygameの初期化
+    pg.init()
 
-if __name__ == 'main':
-  main()
-  
+    # 画面作成
+    win = pg.display.set_mode((WIDTH, HEIGHT))
+
+    # クロックを生成
+    clock = pg.time.Clock()
+
+    # イベントループ
+    while True:
+      for e in pg.event.get():
+        if e.type == pg.QUIT:
+          pg.quit()
+          sys.exit()
+
+        # 背景を水色に塗りつぶす
+        win.fill((135, 206, 235))
+
+        # 画面を更新
+        pg.display.flip()
+
+if __name__ == '__main__':
+    main()
