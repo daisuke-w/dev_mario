a1a460e6d4934fb3c233bf50e1facca80dbb0253
daisuke-w <w.daisuke.001@gmail.com>
2024-11-23 18:06:13 +0900

    マリオを画面に描画

--- main.py
+++ main.py
@@ -9,8 +9,17 @@ HEIGHT = 270
 TILE_X = 16
 TILE_Y = 14
 
+class Mario(pg.sprite.Sprite):
+  ''' マリオのクラス
+  '''
+  def __init__(self):
+    pg.sprite.Sprite.__init__(self)
+
+    self.image = pg.image.load('images/mario_001.png')
+    self.rect = pg.Rect(150, 200, 20, 20)
+
 def main():
-    '''メイン関数
+    ''' メイン関数
     '''
     # Pygameの初期化
     pg.init()
@@ -21,6 +30,13 @@ def main():
     # クロックを生成
     clock = pg.time.Clock()
 
+    # スプライトグループを定義
+    group = pg.sprite.RenderUpdates()
+    # マリオクラスを構築
+    mario = Mario()
+    # マリオをグループに追加
+    group.add(mario)
+
     # イベントループ
     while True:
       for e in pg.event.get():
@@ -30,6 +46,12 @@ def main():
 
         # 背景を水色に塗りつぶす
         win.fill((135, 206, 235))
+
+        # グループを更新
+        group.update()
+
+        # グループを描画
+        group.draw(win)
 
         # 画面を更新
         pg.display.flip()
