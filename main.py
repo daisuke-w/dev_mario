a869015528b3d1f7a6f726c4e822725763bdaccd
daisuke-w <w.daisuke.001@gmail.com>
2024-11-25 21:15:33 +0900

    リファクタリング

--- main.py
+++ main.py
@@ -10,49 +10,42 @@ TILE_X = 16
 TILE_Y = 14
 
 class Mario(pg.sprite.Sprite):
-  ''' マリオのクラス
-  '''
-  def __init__(self):
-    pg.sprite.Sprite.__init__(self)
+    ''' マリオのクラス '''
+    def __init__(self):
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.image.load('images/mario_001.png')
+        self.rect = pg.Rect(150, 200, 20, 20)
 
-    self.image = pg.image.load('images/mario_001.png')
-    self.rect = pg.Rect(150, 200, 20, 20)
+    def __right(self):
+        self.rect.x += 5
 
-  def __right(self):
-    self.rect.x += 5
-  
-  def __left(self):
-    self.rect.x -= 5
+    def __left(self):
+        self.rect.x -= 5
 
-  def update(self):
-    # キーボードの状態を取得
-    keys = pg.key.get_pressed()
-    if keys[pg.K_RIGHT]:
-      self.__right()
-    if keys[pg.K_LEFT]:
-      self.__left()
+    def update(self):
+        # キーボードの状態を取得
+        keys = pg.key.get_pressed()
+        if keys[pg.K_RIGHT]:
+            self.__right()
+        if keys[pg.K_LEFT]:
+            self.__left()
 
 class Kuriboh(pg.sprite.Sprite):
-  ''' クリボーのクラス
-  '''
-  def __init__(self):
-    pg.sprite.Sprite.__init__(self)
-
-    self.image = pg.image.load('images/kuriboh_001.png')
-    self.rect = pg.Rect(180, 200, 20, 20)
+    ''' クリボーのクラス '''
+    def __init__(self):
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.image.load('images/kuriboh_001.png')
+        self.rect = pg.Rect(180, 200, 20, 20)
 
 class Nokonoko(pg.sprite.Sprite):
-  ''' ノコノコのクラス
-  '''
-  def __init__(self):
-    pg.sprite.Sprite.__init__(self)
-
-    self.image = pg.image.load('images/nokonoko_001.png')
-    self.rect = pg.Rect(200, 200, 20, 20)
+    ''' ノコノコのクラス '''
+    def __init__(self):
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.image.load('images/nokonoko_001.png')
+        self.rect = pg.Rect(200, 200, 20, 20)
 
 def main():
-    ''' メイン関数
-    '''
+    ''' メイン関数 '''
     # Pygameの初期化
     pg.init()
     # イベント実行フラグ
@@ -66,41 +59,36 @@ def main():
 
     # スプライトグループを定義
     group = pg.sprite.RenderUpdates()
-    # マリオクラスを構築
+    # 各スプライトを構築してグループに追加
     mario = Mario()
-    # クリボークラスを構築
     kuriboh = Kuriboh()
-    # ノコノコクラスを構築
     nokonoko = Nokonoko()
-    # マリオをグループに追加
-    group.add(mario)
-    # クリボーをグループに追加
-    group.add(kuriboh)
-    # ノコノコをグループに追加
-    group.add(nokonoko)
+    group.add(mario, kuriboh, nokonoko)
 
     # イベントループ
     while running:
-      for e in pg.event.get():
-        if e.type == pg.QUIT:
-          running = False
-          pg.quit()
-          sys.exit()
+        # イベント処理
+        for e in pg.event.get():
+            if e.type == pg.QUIT:
+                running = False
 
-      # 背景を水色に塗りつぶす
-      win.fill((135, 206, 235))
+        # 背景を水色に塗りつぶす
+        win.fill((135, 206, 235))
 
-      # グループを更新
-      group.update()
+        # グループの更新
+        group.update()
 
-      # グループを描画
-      group.draw(win)
+        # グループの描画
+        group.draw(win)
 
-      # 画面を更新
-      pg.display.flip()
+        # 画面を更新
+        pg.display.flip()
 
-      # フレームレートを設定
-      clock.tick(30)
+        # フレームレートを設定
+        clock.tick(30)
+
+    pg.quit()
+    sys.exit()
 
 if __name__ == '__main__':
     main()
