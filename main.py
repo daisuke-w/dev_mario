0d2f07bcf5080f4e2ebee5e9dacd46d6abe66a85
daisuke-w <w.daisuke.001@gmail.com>
2024-11-29 20:48:01 +0900

    ノコノコ移動

--- main.py
+++ main.py
@@ -121,6 +121,18 @@ class Nokonoko(pg.sprite.Sprite):
         self.image = pg.image.load('images/nokonoko_001.png')
         self.rect = pg.Rect(200, 200, 20, 20)
 
+        # 横方向の速度
+        self.__vx = 2
+
+    def update(self):
+        # X方向に移動
+        self.rect.x += self.__vx
+
+        # 画面端で反転
+        if self.rect.x <= 0 or self.rect.x >= WIDTH - self.rect.width:
+            # 方向を反転
+            self.__vx = -self.__vx
+
 def main():
     ''' メイン関数 '''
     # Pygameの初期化
