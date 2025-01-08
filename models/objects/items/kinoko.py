e1795548c8907a01d06a177627c4a38998976742
daisuke-w <w.daisuke.001@gmail.com>
2025-01-08 19:35:57 +0900

    item出現アニメーション調整

--- models/objects/items/kinoko.py
+++ models/objects/items/kinoko.py
@@ -14,11 +14,21 @@ class Kinoko(Item):
         images = [
             pg.image.load('images/kinoko_001.png')
         ]
-        super().__init__(images, x, y, -2, 'kinoko', player)
+        super().__init__(images, x, y, 2, 'kinoko', player)
         self.vx = 2
+        # アイテム生成時の初期高さ格納用
+        self.initial_top = 0
 
     def update(self, dt=0):
         super().update()
+        if self.active:
+            # 上昇して出現するアニメーション
+            self.rect.y -= self.vy
+            if self.initial_top - self.rect.y == self.appear_distance:
+                self.vy = 0
+                self.active = False
+                self.on_block = False
+
         # 出現アニメーション終了後の動作
         if not self.active:
             self.rect.x += self.vx
