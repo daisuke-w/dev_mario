2331b7923dbfce85a8bc17a1cd6e015773a16632
daisuke-w <w.daisuke.001@gmail.com>
2025-01-30 21:57:14 +0900

    yamlマッピング用クラスの作成

---
+++ configs/config_definition.py
@@ -0,0 +1,41 @@
+from pydantic import BaseModel
+
+
+class DisplayConfig(BaseModel):
+    width: int
+    height: int
+    tile_x: int
+    tile_y: int
+    tile_size: int
+    background: list[int]
+
+class GameConfig(BaseModel):
+    frame_rate: int
+    animation_interval: int
+    invincibility_duration: int
+    gravity: int
+    walk_speed: int
+    jump_speed: int
+    shell_speed: int
+    dead_jump_height: int
+    dead_anime_counter: int
+
+class BlockTypesConfig(BaseModel):
+    ground: int
+    wall: int
+    block: int
+    hatena_block: int
+    hatena_block_released: int
+
+class BlockFragmentsConfig(BaseModel):
+    horizontal_speed_range: list[int]
+    vertical_speed_range: list[int]
+
+class BlockConfig(BaseModel):
+    types: BlockTypesConfig
+    block_fragments: BlockFragmentsConfig
+
+class AllConfig(BaseModel):
+    display: DisplayConfig
+    game: GameConfig
+    block: BlockConfig
